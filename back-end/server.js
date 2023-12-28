0ad823d68ea223d99400a98ac1663a0ccaea4310
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-12-28 21:07:02 +0530

    Add server.js

---
+++ back-end/server.js
@@ -0,0 +1,9 @@
+const express  = require("express");
+const mongose = require("mongose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = require("app");
+
+
+
