4ac5f876603ca9600aeb312519eb6b5f8de52d82
shehanKavindu <kavindushehansayakkara@gmail.com>
2024-01-04 00:07:43 +0530

    Update

--- back-end/routes/students.js
+++ back-end/routes/students.js
@@ -2,7 +2,20 @@ const routes = require("express").Router();
 const { Router } = require("express");
 let Student = require("../models/student");
 
-Router.routes("../add").post((res,req)=>{
+Router.routes("../add").post((req,res)=>{
+    const name = req. body. name;
+    const age = Number(req .body . age);
+    const gender = req.body.gender;
+    
+      
+    const newStudent = new Student({
+        name,
+        age,
+        gender
+    })
+    
 
 
-}
+
+
+})
