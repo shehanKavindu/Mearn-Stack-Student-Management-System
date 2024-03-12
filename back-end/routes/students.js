615354cfbbbeea3e2ebd26a3e218fedd35a1a93f
shehanKavindu <kavindushehansayakkara@gmail.com>
2024-03-12 22:59:08 +0530

    update

--- back-end/routes/students.js
+++ back-end/routes/students.js
@@ -62,3 +62,10 @@ Router.routes("/Delete/:id ").delete(async(req,res)=>{
     })
     Router.routes("/get/:id").get(async(req,res))
     
+    
+module.exports=routes;
+module.exports=routes; 
+
+
+  
+
